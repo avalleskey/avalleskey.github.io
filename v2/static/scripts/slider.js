ab7c958c9f2d704d56c141ce29c4339d76dfe453
Austin Valleskey <austin.valleskey@gmail.com>
2016-12-20 16:15:15 -0600

    Last push before live

--- v2/static/scripts/slider.js
+++ v2/static/scripts/slider.js
@@ -16,10 +16,25 @@ var resizeTimer;
 $(window).on('resize', function(e) {
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(function() {
-    updateSlideshow();
+    fixSizing();
   }, 50);
 
 });
+
+function fixSizing() {
+  var width = document.body.clientWidth;
+
+  if (width > 1050) {
+    $(".speakingSlideshow .slideshowSlides").css("left", "calc(-1 * (((50vw - 15px - 120px) * " + currentSlide + ") + (2px * " + currentSlide + ")) + 60px)");
+  }
+  else if (width <= 1050 && width > 960) {
+    $(".speakingSlideshow .slideshowSlides").css("left", "calc(-1 * (((50vw - 15px - 80px) * " + currentSlide + ") + (2px * " + currentSlide + ")) + 40px)");
+  }
+  else {
+    // width <= 960
+    $(".speakingSlideshow .slideshowSlides").css("left", "calc(-1 * (((100vw - 20px - 60px) * " + currentSlide + ") + (2px * " + currentSlide + ")) + 30px)");
+  }
+}
 
 function updateSlideshow() {
   if (currentSlide == 0) {
@@ -47,7 +62,7 @@ function updateSlideshow() {
   }
   else {
     // width <= 960
-    $(".speakingSlideshow .slideshowSlides").css("left", "calc(-1 * (((100vw - 40px - 80px) * " + currentSlide + ") + (2px * " + currentSlide + ")) + 40px)");
+    $(".speakingSlideshow .slideshowSlides").css("left", "calc(-1 * (((100vw - 20px - 60px) * " + currentSlide + ") + (2px * " + currentSlide + ")) + 30px)");
   }
   $(".speakingSlideshow .slideshowSlides .currentSlideshowSlide").each( function() {
     $(this).removeClass("currentSlideshowSlide");
