175772ce33f6413aad87dcefacceb28f37e64336
Austin Valleskey <austin.valleskey@gmail.com>
2016-12-19 19:00:39 -0600

    Updated Design

--- v2/static/scripts/main.js
+++ v2/static/scripts/main.js
@@ -3,6 +3,18 @@ $(document).ready( function() {
     $("body").addClass("ready");
     setTimeout( function() {
       $("body").addClass("loaded");
-    }, 800);
+      setTimeout( function() {
+        $(".rightSideNav ul li[name='mywork']").addClass("activeNavItem");
+      }, 600);
+    }, 600);
   }, 500);
+});
+$(".rightSideNav ul li").click( function() {
+  var name = $(this).attr("name");
+  if (name != "work") {
+    $(".rightSideNav ul li").each( function() {
+      $(this).removeClass("activeNavItem");
+    });
+    $(this).addClass("activeNavItem");
+  }
 });
